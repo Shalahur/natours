91afeda71aee4fe1c2213f70c2fa3315301926dc
Shalahur <srpolash1991@gmail.com>
2025-03-10 11:30:03 +0600

    Create document(insert row)

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('./../model/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Missing name or price.',
-    });
-  }
-  next();
-};
-
 /**** Route handler ****/
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -49,11 +39,19 @@ exports.deleteTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tours: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tours: newTour,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
