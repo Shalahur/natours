6de37e7bb16133211b6687c1e38b3c8d347f5a46
Shalahur <srpolash1991@gmail.com>
2025-03-10 11:02:12 +0600

    Create model and remove hardcode codes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,13 +3,15 @@ const tourController = require('./../controller/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkId);
+// router.param('id', tourController.checkId);
 
-router.route('/')
+router
+  .route('/')
   .get(tourController.getAllTours)
   .post(tourController.checkBody, tourController.createTour);
 
-router.route('/:id')
+router
+  .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
   .delete(tourController.deleteTour);
