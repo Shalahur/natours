48347430deb4067ab564a002460898dfabe1be69
Shalahur <srpolash1991@gmail.com>
2025-03-11 13:13:34 +0600

    Aliasing the api

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,6 +1,13 @@
 const Tour = require('./../model/tourModel');
 
 /**** Route handler ****/
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingAverage,summary';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // 1:Filtering
