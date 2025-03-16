aa2980260934ea8d68cb2a5660a373e62adb822a
Shalahur <srpolash1991@gmail.com>
2025-03-16 12:11:15 +0600

    Add global error handler

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-//Custom middleware
-app.use((req, res, next) => {
-  console.log('Hello from the middleware.');
-  next();
-});
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -27,5 +21,28 @@ app.use((req, res, next) => {
 /**** Routes ****/
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
